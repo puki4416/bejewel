b48b3e06d3f8ccb1905e085a8bc8c7290ae6a4b1
김상두 <puki4416@gmail.com>
2022-12-16 21:06:45 +0900

    chore : json-sever 세팅

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./server/db.json");
+const middlewares = jsonServer.defaults({});
+
+const PORT = 8080;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`Mock Server is running (PORT: ${PORT})`);
+});
